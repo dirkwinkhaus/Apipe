94854a38212e325f9097d864d59b1b48de53f353
Dirk Winkhaus <dirk@googlemail.com>
2017-08-20 21:48:36 +0200

    add first step for config test

---
+++ test/Unit/Config/ApipeConfigTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Apipe\Config;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Class ApipeConfigTest
+ * @package Apipe\Config
+ */
+class ApipeConfigTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShould()
+    {
+        $apipeConfig =
+            new ApipeConfig(
+                $this->getFileSystemProphecy()->reveal(),
+                $this->getConfigDissolverProphecy()->reveal(),
+                $this->getConfig()
+            );
+
+
+    }
+
+    /**
+     * @return ObjectProphecy
+     */
+    private function getFileSystemProphecy(): ObjectProphecy
+    {
+        return $this->prophesize(Filesystem::class);
+    }
+
+    /**
+     * @return ObjectProphecy
+     */
+    private function getConfigDissolverProphecy(): ObjectProphecy
+    {
+        return $this->prophesize(ConfigDissolver::class);
+    }
+
+    /**
+     * @return array
+     */
+    private function getConfig()
+    {
+        return [];
+    }
+}
